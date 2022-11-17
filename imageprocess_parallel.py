3086a438c21bb00e4041454773a01ba9ce8eb7c6
Andre geldenhuis <andre.geldenhuis@vuw.ac.nz>
2022-11-17 18:44:52 +1300

    Updated imageprocess_parallel to reflect moving the functions to their own library

--- imageprocess_parallel.py
+++ imageprocess_parallel.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from PIL import Image, ImageDraw
-from imagefunctions import imageprocess
+from library.imagefunctions import imageprocess
 
 test_path = 'test_data'
 output_path = 'output'
@@ -11,8 +11,6 @@ raw_path = test_path
 # Generate Path objects
 raw_path = Path.cwd().joinpath('test_data','images')
 imfiles = raw_path.glob("*.JPG")
-
-
 
 # Print filepath we're working on and process filename
 for filepath in imfiles:
